e5c2bcb3966f909facedc245aa02d64c3f12fb35
Gabriel Ong <ryer168@gmail.com>
2024-02-28 16:26:59 -0800

    update rating config

--- functions/utils/calculateAveragePlayerStats.ts
+++ functions/utils/calculateAveragePlayerStats.ts
@@ -1,6 +1,16 @@
+import { round } from 'lodash';
+import {
+  movedDown,
+  movedDownExtra,
+  movedUp,
+  movedUpExtra,
+  calculateRating,
+  mapRatingToString,
+  ratingThresholds,
+  roundToNearestThreshold
+} from './ratingUtils';
 import { getAmountToAverage } from './getAmountToAverage';
 import { GameData, LeagueData, PlayerData } from '../types';
-import { round } from 'lodash';
 
 /**
  * @description complex check for valid stats. ortg and drtg are sometimes 0, but those are not valid numbers
@@ -16,54 +26,6 @@ const isValidStatline = (stat: string, value: any): boolean => {
       !(stat === 'ortg' && value === 0) &&
       !(stat === 'drtg' && value === 0)
   );
-};
-
-const ratingThresholds = [1, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 9];
-const movedUp = '(↑)';
-const movedDown = '(↓)';
-const movedUpExtra = '(↑↑)';
-const movedDownExtra = '(↓↓)';
-
-const RATING_CONFIG = {
-  Bench: 3.5,
-  Starter: 6.5,
-  AllStar: 7.5,
-  /** Highest value is 10 */
-  MVP: 10
-};
-
-const mapRatingToString = (value: number): string => {
-  const rating = Object.keys(RATING_CONFIG)
-    .sort((a, b) => RATING_CONFIG[a] - RATING_CONFIG[b])
-    .find((key) => value <= RATING_CONFIG[key]);
-
-  return rating || 'MVP';
-};
-
-const calculateRating = (PER: number): number => {
-  // Take a PER from 0 to 35+ and convert it to a 0-10 scale. a PER of 15 is always 5
-  let rating = 10;
-  if (PER <= 0) {
-    rating = 0;
-  }
-  if (PER <= 15) {
-    rating = (PER / 15) * 5;
-  }
-  if (PER <= 35) {
-    rating = ((PER - 15) / 20) * 5 + 5;
-  }
-
-  return rating;
-};
-
-const roundToNearestThreshold = (rating: number): number => {
-  let closest = ratingThresholds[0];
-  for (let i = 1; i < ratingThresholds.length; i++) {
-    if (Math.abs(ratingThresholds[i] - rating) < Math.abs(closest - rating)) {
-      closest = ratingThresholds[i];
-    }
-  }
-  return closest;
 };
 
 export const calculateAveragePlayerStats = (
---
+++ functions/utils/ratingUtils.ts
@@ -0,0 +1,52 @@
+export const movedUp = '(↑)';
+export const movedDown = '(↓)';
+export const movedUpExtra = '(↑↑)';
+export const movedDownExtra = '(↓↓)';
+
+const RATING_CONFIG = {
+  Bench: 4.3,
+  Rotation: 5,
+  Starter: 5.4,
+  SecondOption: 6.5,
+  AllStar: 7.5,
+  /** Highest value is 10 */
+  MVP: 10
+};
+
+export const mapRatingToString = (value: number): string => {
+  const rating = Object.keys(RATING_CONFIG)
+    .sort((a, b) => RATING_CONFIG[a] - RATING_CONFIG[b])
+    .find((key) => value <= RATING_CONFIG[key]);
+
+  return rating || 'MVP';
+};
+
+export const ratingThresholds = [0, 2.5, 5, 7.5, 10];
+
+export const calculateRating = (PER: number): number => {
+  // Take a PER from 0 to 35+ and convert it to a 0-10 scale. a PER of 15 is always 5
+  let rating = 10;
+  if (PER <= 0) {
+    rating = 0;
+  }
+  if (PER <= 15) {
+    rating = (PER / 15) * 5;
+  }
+  if (PER <= 35) {
+    rating = ((PER - 15) / 20) * 5 + 5;
+  }
+
+  return rating;
+};
+
+export const roundToNearestThreshold = (rating: number): number => {
+  let closest = ratingThresholds[0];
+  for (let i = 1; i < ratingThresholds.length; i++) {
+    if (Math.abs(ratingThresholds[i] - rating) < Math.abs(closest - rating)) {
+      closest = ratingThresholds[i];
+    }
+  }
+  return closest;
+};
+
+export default {};
