8cbae888ea51e6509861c3bbe8f9d5735d2f6083
Gabriel Ong <ryer168@gmail.com>
2024-03-15 17:53:01 -0700

    add positional refactor to player api

--- functions/api/fetchPlayerData.ts
+++ functions/api/fetchPlayerData.ts
@@ -1,29 +1,82 @@
 import admin from 'firebase-admin';
+import { error } from 'firebase-functions/logger';
 import { Response } from 'express';
-
-interface IPlayerData {
-  [key: string]: any; // replace any with the actual type
-}
+import { GameData, LeagueData, PlayerData } from '../types';
+import { calculateAveragePlayerStats } from '../utils';
 
 // * For individual player pages
 const fetchPlayerData = async (req: any, res: Response): Promise<void> => {
-  const { playerID } = req.query;
+  const { playerID, position } = req.query;
 
   if (!playerID || typeof playerID !== 'string') {
-    throw new Error('Invalid parameter passed');
+    res.status(400).send('Invalid playerId parameter passed');
+    throw new Error('Invalid playerId parameter passed');
+  }
+
+  if (position && (position > 5 || position < 1)) {
+    res.status(400).send('Invalid position parameter passed');
+    throw new Error('Invalid position parameter passed');
   }
 
   const db = admin.firestore();
-  const playerData: IPlayerData | undefined = await db
+  const playerData: PlayerData = await db
     .collection('players')
     .doc(playerID)
     .get()
     .then((doc) => {
       // * undefined if player does not exist
-      return doc.data();
+      return doc.data() as PlayerData;
     });
 
-  res.send(playerData);
+  if (!playerData?.gp) {
+    error('No games played for this player');
+    res.status(404).send('No games played for this player');
+    return;
+  }
+
+  let response: PlayerData = playerData;
+
+  // * Recalculate player averages based on position
+  if (position) {
+    const gameDataRef = await db.collection('games').where('name', 'in', playerData.alias).get();
+    const gameData = gameDataRef.docs
+      .map((doc) => doc.data() as GameData)
+      .filter((game) => game.isAI !== 1 && game.pos === position);
+
+    // * Get league data to recalculate PER based on league averages, plus use for comparison analysis
+    const leagueData = await db
+      .collection('league')
+      .orderBy('createdAt', 'desc')
+      .limit(1)
+      .get()
+      .then((querySnapshot) => {
+        const league: LeagueData[] = [];
+        querySnapshot.forEach((doc) => {
+          league.push(doc.data() as LeagueData);
+        });
+        return league[0];
+      });
+
+    if (gameData.length) {
+      const avgPlayerStats = calculateAveragePlayerStats(
+        leagueData,
+        gameData,
+        playerData.name,
+        playerData.alias,
+        playerData.ftPerc,
+        playerData.rating,
+        playerData.gpSinceLastRating
+      );
+
+      response = avgPlayerStats;
+    } else {
+      error('No games found for this player');
+      res.status(404).send('No games found for this player');
+      return;
+    }
+  }
+
+  res.send(response);
 };
 
 export default fetchPlayerData;
