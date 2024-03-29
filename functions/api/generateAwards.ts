08a4e64be76fc71585504718beb1c8efb1eb7618
Gabriel Ong <ryer168@gmail.com>
2024-03-29 15:38:02 -0700

    fix incorrect reducer defaults

--- functions/api/generateAwards.ts
+++ functions/api/generateAwards.ts
@@ -15,22 +15,27 @@ export const generateAwards = async (req: Request, res: Response) => {
 
     const db = admin.firestore();
     const querySnapshot = await db.collection('players').get();
-    const playerList: iPlayerData[] = [];
+    const rawPlayerList: iPlayerData[] = [];
     querySnapshot.docs.forEach((doc) => {
       const playerData = doc.data() as PlayerData;
       if (playerData?.gp > 1) {
         const player: iPlayerData = { ...playerData, id: doc.id };
-        playerList.push(player);
+        rawPlayerList.push(player);
       }
     });
 
     // * Set the two most played positions for each player as the topPositions array
-    playerList.forEach((player) => {
+    rawPlayerList.forEach((player) => {
       const sortedPositions = Object.entries(player.positions || {})
         .sort(([, a], [, b]) => b - a)
         .map(([key]) => key);
       player.topPositions = [sortedPositions[0], sortedPositions[1]];
     });
+
+    const playerList = rawPlayerList.sort((a, b) => b.PER - a.PER);
+    const minGamesPlayerList = playerList.filter((player) => player.gp >= 25);
+    const minFGAPlayerList = playerList.filter((player) => player.fga * player.gp >= 300);
+    const min3PAPlayerList = playerList.filter((player) => player.threepa * player.gp >= 82);
 
     // * Get the poaDefender sub collection for each player, and the document with the id lock from that sub collection
     // * if the lock document does not exist, skip this player
@@ -59,20 +64,20 @@ export const generateAwards = async (req: Request, res: Response) => {
     );
 
     // * MVP is the player with the highest PER with a minimum of 25 games played
-    const mvp = playerList.reduce((prev, current) => {
+    const mvp = minGamesPlayerList.reduce((prev, current) => {
       if (prev.aPER && current.aPER && current.gp >= 25) {
         return prev.aPER > current.aPER ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, minGamesPlayerList[0]);
 
     // * DPOY is the player with the lowest drtg with a minimum of 25 games played
-    const dpoy = playerList.reduce((prev, current) => {
+    const dpoy = minGamesPlayerList.reduce((prev, current) => {
       if (current.drtg && current.gp >= 25) {
         return prev.drtg < current.drtg ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, minGamesPlayerList[0]);
 
     // * POA Defender is the player with the lowest drtg in their lock sub collection with a minimum of 25 games played
     const poaDefender = poaDefenders.reduce((prev, current) => {
@@ -83,7 +88,7 @@ export const generateAwards = async (req: Request, res: Response) => {
     }, poaDefenders[0]);
 
     // * Best shooter is the player with the highest 3P% weighted 40/40/20 with 3PA and high three point attempt rate, min 82 3PA
-    const bestShooter = playerList.reduce((prev, current) => {
+    const bestShooter = min3PAPlayerList.reduce((prev, current) => {
       if (
         current.threePerc &&
         current.threepAR &&
@@ -96,10 +101,10 @@ export const generateAwards = async (req: Request, res: Response) => {
         return prevWeighted > currentWeighted ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, min3PAPlayerList[0]);
 
     // * Worst shooter is the player with the lowest 3P% weighted 80/20 with three point attempt rate, min 82 3PA
-    const worstShooter = playerList.reduce((prev, current) => {
+    const worstShooter = min3PAPlayerList.reduce((prev, current) => {
       if (
         current.threePerc &&
         prev.threePerc &&
@@ -111,91 +116,88 @@ export const generateAwards = async (req: Request, res: Response) => {
         return prevWeighted < currentWeighted ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, min3PAPlayerList[0]);
 
     // * Most active player is the player with the most games played
-    const mostActive = playerList.reduce((prev, current) => {
+    const mostActive = minGamesPlayerList.reduce((prev, current) => {
       return prev.gp > current.gp ? prev : current;
-    }, playerList[0]);
+    }, minGamesPlayerList[0]);
 
     // * Most used player is the player with the highest USG%, min 25 games
-    const mostUsed = playerList.reduce((prev, current) => {
+    const mostUsed = minGamesPlayerList.reduce((prev, current) => {
       if (prev.usageRate && current.usageRate && current.gp >= 25) {
         return prev.usageRate > current.usageRate ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, minGamesPlayerList[0]);
 
     // * Least used player is the player with the lowest USG%, min 25 games
-    const leastUsed = playerList.reduce((prev, current) => {
+    const leastUsed = minGamesPlayerList.reduce((prev, current) => {
       if (current.gp && current.gp >= 25) {
         return prev.usageRate < current.usageRate ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, minGamesPlayerList[0]);
 
     // * Most efficient player is the player with the highest EFG%, min 300 FGA
-    const mostEfficient = playerList.reduce((prev, current) => {
+    const mostEfficient = minFGAPlayerList.reduce((prev, current) => {
       if (prev.efgPerc && current.efgPerc && current.fga * current.gp >= 300) {
         return prev.efgPerc > current.efgPerc ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, minFGAPlayerList[0]);
 
     // * Least efficient player is the player with the lowest EFG%, min 300 FGA
-    const leastEfficient = playerList.reduce((prev, current) => {
+    const leastEfficient = minFGAPlayerList.reduce((prev, current) => {
       if (prev.efgPerc && current.efgPerc && current.fga * current.gp >= 300) {
         return prev.efgPerc < current.efgPerc ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, minFGAPlayerList[0]);
 
     // * Shot chucker is the player with the lowest EFG% weighted 50/50 with high FGA, min 300 FGA
-    const shotChucker = playerList.reduce((prev, current) => {
+    const shotChucker = minFGAPlayerList.reduce((prev, current) => {
       if (prev.efgPerc && current.efgPerc && current.fga * current.gp >= 300) {
         const prevWeighted = prev.efgPerc * 0.5 + prev.fga * 0.5;
         const currentWeighted = current.efgPerc * 0.5 + current.fga * 0.5;
         return prevWeighted < currentWeighted ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, minFGAPlayerList[0]);
 
     // * Fastbreak player is the player with the highest pace weighted 80/20 with FGA, min 25 games
-    const fastbreakPlayer = playerList.reduce((prev, current) => {
+    const fastbreakPlayer = minGamesPlayerList.reduce((prev, current) => {
       if (prev.gp && current.gp && current.gp >= 25) {
         const prevWeighted = prev.pace * 0.8 + prev.fga * 0.2;
         const currentWeighted = current.pace * 0.8 + current.fga * 0.2;
         return prevWeighted > currentWeighted ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, minGamesPlayerList[0]);
 
     // * Most attacked player is the player with the most OFGA, min 300 OFGA
-    const mostAttacked = playerList.reduce((prev, current) => {
+    const mostAttacked = minFGAPlayerList.reduce((prev, current) => {
       if (prev.oFGA && current.oFGA && current.oFGA * current.gp >= 300) {
         return prev.oFGA > current.oFGA ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, minFGAPlayerList[0]);
 
     // * Best intimidator is the player with the lowest OFG% weighted 50/50 with OFGA, min 300 OFGA
-    const bestIntimidator = playerList.reduce((prev, current) => {
+    const bestIntimidator = minFGAPlayerList.reduce((prev, current) => {
       if (prev.oEFGPerc && current.oEFGPerc && current.oFGA && current.oFGA * current.gp >= 300) {
         const prevWeighted = prev.oEFGPerc * 0.5 + prev.oFGA * 0.5;
         const currentWeighted = current.oEFGPerc * 0.5 + current.oFGA * 0.5;
         return prevWeighted < currentWeighted ? prev : current;
       }
       return prev;
-    }, playerList[0]);
+    }, minFGAPlayerList[0]);
 
     // * All NBA First team is the top 5 players with the highest PER, min 25 games
-    const sortedPlayers = playerList
-      .filter((player) => player.gp && player.gp >= 25)
-      .sort((a, b) => b.aPER - a.aPER);
-    const allNBAFirst = sortedPlayers.slice(0, 5);
+    const allNBAFirst = minFGAPlayerList.slice(0, 5);
 
     // * All NBA Second team is the top 5 players with the highest PER after allNBAFirst, min 25 games
-    const allNBASecond = sortedPlayers.slice(5, 10);
+    const allNBASecond = minFGAPlayerList.slice(5, 10);
 
     const awards: Award = {
       mvp: { id: mvp.id, name: mvp.name, value: mvp.aPER, positions: mvp.topPositions },
