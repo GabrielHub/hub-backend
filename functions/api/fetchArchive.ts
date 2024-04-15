e4559f9c19d40cb9c9c22b774064936b109dd94e
GabrielHub <ryer168@gmail.com>
2024-04-15 16:08:02 -0400

    add unique id

--- functions/api/fetchArchive.ts
+++ functions/api/fetchArchive.ts
@@ -4,30 +4,30 @@ import { Request, Response } from 'express';
 import dayjs from 'dayjs';
 
 export const fetchArchive = async (req: Request, res: Response): Promise<Response<any>> => {
-    const { year } = req.query;
+  const { year } = req.query;
 
-    if (!year || typeof year !== 'string') {
-        return res.status(400).send('Invalid year passed');
-    }
+  if (!year || typeof year !== 'string') {
+    return res.status(400).send('Invalid year passed');
+  }
 
-    if (!dayjs(year, 'YYYY').isValid()) {
-        return res.status(400).send('Invalid year format');
-    }
+  if (!dayjs(year, 'YYYY').isValid()) {
+    return res.status(400).send('Invalid year format');
+  }
 
-    const db = admin.firestore();
-    const archiveCollection = `playersArchive-${year}`;
-    // * Archived data could be different from year to year, so type has to be any[]
-    let playerData: any[] = [];
-    try {
-        const snapshot = await db.collection(archiveCollection).get();
-        snapshot.forEach((doc) => {
-            playerData.push(doc.data());
-        });
-        return res.status(200).send(playerData);
-    } catch (err) {
-        error(err);
-        return res.status(500).send('Error fetching archive data');
-    }
-}
+  const db = admin.firestore();
+  const archiveCollection = `playersArchive-${year}`;
+  // * Archived data could be different from year to year, so type has to be any[]
+  const playerData: any[] = [];
+  try {
+    const snapshot = await db.collection(archiveCollection).get();
+    snapshot.forEach((doc) => {
+      playerData.push({ ...doc.data(), id: doc.id });
+    });
+    return res.status(200).send(playerData);
+  } catch (err) {
+    error(err);
+    return res.status(500).send('Error fetching archive data');
+  }
+};
 
-export default {}
+export default {};
